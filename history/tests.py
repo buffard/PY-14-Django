cb616ec39e869dfbf0252e29ef18ca347c33290c
buffard <samuelwebber19@gmail.com>
2019-01-24 10:06:33 -0600

    got rid of a half finished test

--- history/tests.py
+++ history/tests.py
@@ -1,8 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
-def test_list_artists(self):
-  new_artist = Artists.objects.create(
-    name="Suzy Saxophone"
-    birth_date="12/25/58"
-  )
+# 
